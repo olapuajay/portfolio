185bf0eb623d4d1bc8934b3d2aadacc9a2df044c
Olapu Ajay <olapuajay@gmail.com>
2025-02-02 10:06:42 +0530

    Project setup with vite+react and tailwind CSS

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='text-center bg-gray-100 h-screen flex items-center justify-center'>
+      Hello
+    </div>
+  )
+}
+
+export default App
