391c03d6a6721fb9938583640df22e51e3e52976
Olapu Ajay <olapuajay@gmail.com>
2025-02-03 11:55:24 +0530

    Add location and social links to Home component

--- src/App.jsx
+++ src/App.jsx
@@ -9,16 +9,18 @@ function App() {
   const siteProps = {
     name: 'Ajay',
     description: 'I\'m a Web Developer specializing in JavaScript(React.js and Node.js) with a passion for building intuitive, responsive, and visually engaging web experiences. I focus on crafting seamless UI/UX designs that are fast, accessible, and user-friendly.',
+    email: 'olapuajay@gmail.com',
+    location: 'Peddapalli, India',
     socialLinks: [
-      { name: 'Github', url: ''},
-      { name: 'LinkedIn', url: ''},
+      { name: 'github', url: 'https://github.com/olapuajay'},
+      { name: 'linkedin', url: 'https://www.linkedin.com/in/olapuajay/'},
     ],
   }
 
   return (
     <div className="App">
       <Navbar />
-      <Home name={siteProps.name} description={siteProps.description} />
+      <Home name={siteProps.name} description={siteProps.description} location={siteProps.location} socialLinks={siteProps.socialLinks} />
       <About />
       <Skills />
       <Projects />
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Profile from '../assets/profile.png'
 
-function Home({ name, description }) {
+function Home({ name, description, location, socialLinks }) {
   return (
     <div className='flex flex-col-reverse md:flex-row w-full justify-center items-start px-8 pb-10 md:p-15 md:h-[calc(100vh-4rem)]'>
       <div className='md:w-3/4 md:pt-20'>
@@ -13,14 +13,18 @@ function Home({ name, description }) {
         </p>
         <p className='text-[#9CA3AF] text-sm md:text-xl mt-2 md:mt-2'>
           <i className='fas fa-map-marker-alt text-sm md:text-lg mt-2 md:mt-2 mr-2'></i>
-          Peddapalli, India
+          {location}
         </p>
-        <div className='flex justify-items-start space-x-2 items-center mt-4 md:mt-6'>
+        <div className='flex justify-items-start space-x-4 items-center mt-4 md:mt-6'>
           <button className='bg-[#6366F1] py-2 px-4 rounded-xl text-white text-center font-semibold hover:bg-[#637bf1] duration-300 min-w-35 cursor-pointer'>
             Contact Me
           </button>
-          <i className='fab fa-github text-3xl text-[#9CA3AF] hover:text-white duration-300 cursor-pointer'></i>
-          <i className='fab fa-linkedin text-3xl text-[#9CA3AF] hover:text-[#0A66C2] duration-300 cursor-pointer'>          </i>
+          <a href={socialLinks.find(link => link.name === 'github').url}>
+            <i className='fab fa-github text-3xl text-[#9CA3AF] hover:text-white duration-300 cursor-pointer'></i>
+          </a>
+          <a href={socialLinks.find(link => link.name === 'linkedin').url}>
+            <i className='fab fa-linkedin text-3xl text-[#9CA3AF] hover:text-[#0A66C2] duration-300 cursor-pointer'>          </i>
+          </a>
         </div>
       </div>
       <div className='md:w-1/4 md:pt-10 pt-0'>
