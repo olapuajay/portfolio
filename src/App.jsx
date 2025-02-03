41c90e5b0c70fd9ba2286b72878c685b627dc062
Olapu Ajay <olapuajay@gmail.com>
2025-02-03 10:01:53 +0530

    Add Skills component to showcase familiar technologies

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import About from './components/About'
 import Home from './components/Home'
 import Navbar from './components/Navbar'
+import Skills from './components/Skills'
 
 function App() {
   const siteProps = {
@@ -18,6 +19,7 @@ function App() {
       <Navbar />
       <Home name={siteProps.name} description={siteProps.description} />
       <About />
+      <Skills />
     </div>
   )
 }
---
+++ src/components/Skills.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import javascript from '../assets/skills/javascript.png'
+import react from '../assets/skills/react.png'
+import nodejs from '../assets/skills/nodejs.png'
+import tailwindcss from '../assets/skills/tailwind.png'
+import bootstrap from '../assets/skills/bootstrap-framework.png'
+import html from '../assets/skills/html.png'
+import css from '../assets/skills/css.png'
+import git from '../assets/skills/git.png'
+import figma from '../assets/skills/figma.png'
+
+function Skills() {
+  const Skills = [
+    {name: 'Javascript', image: javascript},
+    {name: 'React', image: react},
+    {name: 'Node.js', image: nodejs},
+    {name: 'Tailwind CSS', image: tailwindcss},
+    {name: 'Bootstrap', image: bootstrap},
+    {name: 'HTML', image: html},
+    {name: 'CSS', image: css},
+    {name: 'Git', image: git},
+    {name: 'Figma', image: figma},
+  ]
+  return (
+    <div className='text-center w-full py-5 px-4'>
+      <h2 className="text-xl md:text-3xl font-bold text-center text-white mb-5 ">Skills</h2>
+      <p className='text-[#9CA3AF] text-sm md:text-lg'>The Skills, Tools, and Technologies I'm familiar with</p>
+      <div className='grid grid-cols-3 gap-4 md:grid-cols-6 md:gap-6 w-full  px-8 py-10 '>
+        {Skills.map(skill => (
+          <div key={skill.name} className='flex flex-col items-center'>
+            <img src={skill.image} alt={`Image of ${skill.name}`} className='w-14 h-14 md:w-25 md:h-25 object-contain hover:scale-110 duration-300 cursor-pointer' />
+            <p className='text-[#9CA3AF] text-sm md:text-lg mt-2'>{skill.name}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  )
+}
+
+export default Skills
