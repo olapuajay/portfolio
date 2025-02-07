09a06a1b18e935e6319af9087e8e79a788c444d1
Olapu Ajay <olapuajay@gmail.com>
2025-02-07 12:44:16 +0530

    Remove loading state and component to streamline App structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import { useEffect } from 'react'
 import './App.css'
 import About from './components/About'
 import Contact from './components/Contact'
@@ -8,10 +8,8 @@ import Navbar from './components/Navbar'
 import Projects from './components/Projects'
 import Sidebar from './components/Sidebar'
 import Skills from './components/Skills'
-import Loading from './components/Loading'
 
 function App() {
-  const [loader, setLoader] = useState(true);
   const siteProps = {
     name: 'Ajay',
     description: 'I\'m a Web Developer specializing in JavaScript(React.js and Node.js) with a passion for building intuitive, responsive, and visually engaging web experiences. I focus on crafting seamless UI/UX designs that are fast, accessible, and user-friendly.',
@@ -21,7 +19,7 @@ function App() {
       { name: 'github', url: 'https://github.com/olapuajay'},
       { name: 'linkedin', url: 'https://www.linkedin.com/in/olapuajay/'},
       { name: 'figma', url: 'https://www.figma.com/@olapuajay'},
-      { name: 'twitter', url: 'https://x.com/_ajaypatel_18' },
+      { name: 'twitter', url: 'https://x.com/_ajaypatel_18'},
     ],
   }
 
@@ -32,36 +30,21 @@ function App() {
       disable: 'mobile',
     })
     window.addEventListener('scroll', AOS.refresh);
-
-    const timer = setTimeout(() => {
-      setLoader(false)
-    }, 2000)
     return () => {
-      setTimeout(timer);
       window.removeEventListener('scroll', AOS.refresh);
     }
   }, [])
 
   return (
     <div className="App">
-      {
-        loader ? (
-          <div className='flex justify-center items-center min-h-screen overflow-x-hidden'>
-            <Loading />
-          </div>
-        ) : (
-          <>
-            <Navbar />
-            <Sidebar email={siteProps.email} socialLinks={siteProps.socialLinks} />
-            <Home name={siteProps.name} description={siteProps.description} location={siteProps.location} />
-            <About />
-            <Skills />
-            <Projects />
-            <Contact email={siteProps.email} socialLinks={siteProps.socialLinks} />
-            <Footer name={siteProps.name} />
-          </>
-        )
-      }
+      <Navbar />
+      <Sidebar email={siteProps.email} socialLinks={siteProps.socialLinks} />
+      <Home name={siteProps.name} description={siteProps.description} location={siteProps.location} />
+      <About />
+      <Skills />
+      <Projects />
+      <Contact email={siteProps.email} socialLinks={siteProps.socialLinks} />
+      <Footer name={siteProps.name} />
     </div>
   )
 }
