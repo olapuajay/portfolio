c2012b8b4c4c4b4bc6b7e74bdced3317f509ece0
Olapu Ajay <olapuajay@gmail.com>
2025-02-14 21:05:57 +0530

    Updated the footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ function Footer({ name }) {
   return (
     <div className='w-full bg-gray-900 text-white text-center py-4 px-4'>
       <small className=''>
-        &copy; {new Date().getFullYear()}. All Rights Reserved | Designed & Developed by <span className='text-[#6366F1] uppercase'> {name} </span>
+        &copy; {new Date().getFullYear()} | Designed & Developed by <span className='text-[#6366F1] uppercase'> {name} </span>
       </small>
     </div>
   )
